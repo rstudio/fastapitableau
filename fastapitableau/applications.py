d414f2ef68b2d60aed4ffc02cc2354eee5a3d92f
Toph Allen <toph.allen@gmail.com>
2021-09-13 12:43:06 -0400

    Added info endpoint

--- fastapitableau/applications.py
+++ fastapitableau/applications.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict
 
-from fastapi import FastAPI
+from fastapi import APIRouter, FastAPI, Request
 
 from fastapitableau.openapi import rewrite_tableau_openapi
 
@@ -13,6 +13,7 @@ class FastAPITableau(FastAPI):
         super().__init__()
         self.add_middleware(TableauExtensionMiddleware)
         self.router.route_class = TableauRoute
+        self.include_router(info_router)
 
     def openapi(self) -> Dict[str, Any]:
         if not self.openapi_schema:
@@ -25,3 +26,24 @@ class FastAPITableau(FastAPI):
             openapi_schema = rewrite_tableau_openapi(openapi_schema, tableau_paths)
             self.openapi_schema = openapi_schema
         return self.openapi_schema
+
+
+info_router = APIRouter()
+
+
+@info_router.get("/info")
+def info(request: Request):
+    return {
+        "description": "FastAPITableau API",
+        # "creation_time": "0",
+        # "state_path": "e:\\dev\\server\\server\\server",
+        "server_version": "0.0.1",
+        "name": "Server",  # TODO: Make this use the application name
+        "versions": {
+            "v1": {
+                "features": {
+                    "authentication": {"required": True, "methods": {"basic-auth": {}}}
+                }
+            }
+        },
+    }
