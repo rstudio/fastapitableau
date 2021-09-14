1ecbd2fabe11654ede8fb23db0c9b594320dab99
Toph Allen <toph.allen@gmail.com>
2021-09-14 12:24:40 -0400

    Added warning messages

--- fastapitableau/rstudio_connect.py
+++ fastapitableau/rstudio_connect.py
@@ -1,5 +1,6 @@
 from os import environ, getcwd
 from re import match
+from urllib.parse import urlparse
 
 import requests
 
@@ -19,110 +20,121 @@ def check_rstudio_connect():
     return any(checks)
 
 
-def warning_message():
+def warning_message():  # noqa: C901
     """
     Generate a string of warning messages based on information gathered about our environment in RStudio Connect.
     """
     if not check_rstudio_connect():
         return None
 
+    message_list = []
+
     connect_server = environ.get("CONNECT_SERVER")
     # TODO DEBUG: Print CONNECT_SERVER variable
 
     if connect_server is None:
-        print("Debug message about CONNECT_SERVER not set")
-        pass
+        message_list += (
+            (
+                "### The environment variable `CONNECT_SERVER` is not defined\n"
+                "\n"
+                "Possible Solutions:\n"
+                "\n"
+                "- Have your system administrator confirm `Applications.DefaultServerEnv` is enabled and that `Server.Address` has been defined within the `rstudio-connect.gcfg` file on the RStudio Connect server.,\n"
+                "- Use the application settings for your content within the RStudio Connect dashboard to define the `CONNECT_SERVER` environment variable. It should be set to a reachable https or http address for the server.\n"
+            ),
+        )
         # TODO DEBUG: Add a message if CONNECT_SERVER not defined
-        # TODO MESSAGE: CONNECT_SERVER not defined error message
 
-    # TODO: We won't do that here cos it's a plumber thing??
-    # Parse server to get scheme
-    # If scheme is None:
-    # DEBUG: If server doesn't specify https:// or http://
-    # MESSAGE: CONNECT_SERVER does not specify protocol
+    if urlparse(connect_server.scheme) is None:
+        message_list += (
+            (
+                f"### Environment Variable `CONNECT_SERVER` (value = `{connect_server}` ) does not specify the protocol (`https://` or `http://`)\n"
+                "\n"
+                "Possible Solutions:\n"
+                "\n"
+                "- Have your system administrator confirm that `Server.Address` has been configured with the proper format within the `rstudio-connect.gcfg` file on the RStudio Connect server.\n"
+                "- Use the application settings for your content within the RStudio Connect dashboard to define the CONNECT_SERVER` environment variable with the proper protocol.\n"
+            ),
+        )
+        # DEBUG: If server doesn't specify https:// or http://
 
     connect_api_key = environ.get("CONNECT_API_KEY")
     # TODO
     # DEBUG: Print API key variable
 
-    if connect_server is None:
-        print("Debug message about CONNECT_SERVER not set")
-        pass
+    if connect_api_key is None:
+        message_list += (
+            "### The environment variable `CONNECT_API_KEY` is not defined\n"
+            "\n"
+            "Possible Solutions:\n"
+            "\n"
+            "- Have your administrator enable the `Applications.DefaultAPIKeyEnv` setting within the `rstudio-connect.gcfg` file on the RStudio Connect server.\n"
+            "- Create an API key yourself and use the application settings for your content within the RStudio Connect dashboard to set the the `CONNECT_API_KEY` variable to its value.\n"
+        )
         # TODO
         # DEBUG: Add a message if API key not defined
-        # MESSAGE: API key not defined error message
 
-    # TODO https checking (probably don't need to do)
+    if len(message_list) != 0:
+        messages = "\n\n---\n\n".collapse(message_list)
+        return messages
 
     # call API to see if thing is enabled.
     settings_url = connect_server + "__api__/server_settings"
 
     headers = {"Authorization": "Key " + connect_api_key}
 
-    response = requests.get(settings_url, headers=headers, verify=False)
+    try:
+        response = requests.get(settings_url, headers=headers, verify=False)
+    except Exception as e:
+        message_list += (
+            f"### API request to {connect_server} has failed with error:\n"
+            f"{e}"
+            "\n"
+            "\nPossible Solutions:"
+            "\n"
+            "\n- If you have specified an API key, confirm it is valid."
+            "\n- Confirm there is connectivity between the server itself and the address assigned to it: ",
+            connect_server,
+            "."
+            "\n- If using HTTPS along with self-signed certificates, you may need to allow the FastAPITableau package to use HTTP instead, by setting the environment variable `FASTAPITABLEAU_USE_HTTP` to `True` in the RStudio Connect application settings.",
+        )
 
     if response.status_code != 200:
-        print("Bad status code")
-        # TODO: Message about how we can't get status
+        message_list += (
+            f"### API request to {connect_server} has failed with error: TODO\n"
+            "\n"
+            "Possible Solutions:\n"
+            "\n"
+            "- If you have specified an API_KEY, confirm it is valid.\n"
+            f"- Confirm the server can be reached at {connect_server}.\n"
+            "- TODO: Message about enabling self-signed certificates.\n"
+            "- TODO: There are two error messages in here. One on line 137.\n"
+        )
+        # TODO: DEBUG about how we can't get status
 
     else:
         server_settings = response.json()
         if "tableau_integration_enabled" not in server_settings.keys():
-            print(
-                f"Tableau.IntegrationEnabled is not present within server settings. This Connect server does not support the feature ({server_settings['version']})"
+            message_list += (
+                "### Tableau Integration Feature Flag is not available on the RStudio Connect server. Current server is version: TODO rework\n"
+                "\n"
+                "Possible Solution:\n"
+                "\n"
+                "- Please upgrade to the latest version of RStudio Connect.\n"
             )
-            # TODO: Debug, message.
-
+            # TODO: Debug
         elif server_settings["tableau_integration_enabled"] is False:
-            print("Tableau integration is disabled")
-            # TODO: Message about how setting is disabled.
+            message_list += (
+                "Tableau Integration has been disabled on the RStudio Connect server\n"
+                "\n"
+                "Possible Solution:\n"
+                "\n"
+                "- Please ask your administrator to set `Tableau.TableauIntegrationEnabled` = `true` within `rstudio-connect.gcfg` file on the RStudio Connect server.\n"
+            )
+            # TODO: debug
 
-
-messages = {
-    "connect_server_not_defined": """
-### The environment variable *CONNECT_SERVER* is not defined
-
-Possible Solutions:
-
-- Have your system administrator confirm *Applications.DefaultServerEnv* is enabled and that *Server.Address* has been defined within the *rstudio-connect.gcfg* file on the RStudio Connect server.",
-- Use the application settings for your content within the RStudio Connect dashboard to define the *CONNECT_SERVER* environment variable. It should be set to a reachable https or http address for the server.""",
-    "protocol_not_specified": f"""
-### Environment Variable `CONNECT_SERVER` (value = `{environ.get("CONNECT_SERVER")}` ) does not specify the protocol (`https://` or `http://`)
-
-Possible Solutions:
-
-- Have your system administrator confirm that `Server.Address` has been configured with the proper format within the `rstudio-connect.gcfg` file on the RStudio Connect server.
-- Use the application settings for your content within the RStudio Connect dashboard to define the CONNECT_SERVER` environment variable with the proper protocol.""",
-    "connect_api_key_not_defined": """
-### The environment variable `CONNECT_API_KEY` is not defined
-
-Possible Solutions:
-
-- Have your administrator enable the `Applications.DefaultAPIKeyEnv` setting within the `rstudio-connect.gcfg` file on the RStudio Connect server.
-- Create an API key yourself and use the application settings for your content within the RStudio Connect dashboard to set the the `CONNECT_API_KEY` variable to its value.
-""",
-    "server_responded_with_error": f"""
-### API request to {environ.get("CONNECT_SERVER")} has failed with error:"
-
-Possible Solutions:
-
-- If you have specified an API_KEY, confirm it is valid.
-- Confirm the server can be reached at {environ.get("CONNECT_SERVER")}.
-- TODO: Message about enabling self-signed certificates.
-- TODO: There are two error messages in here. One on line 137.
-""",
-    "tableau_feature_not_available": """
-### Tableau Integration Feature Flag is not available on the RStudio Connect server. Current server is version: TODO rework
-
-Possible Solution:
-
-- Please upgrade to the latest version of RStudio Connect.
-""",
-    "tableau_integration_disabled": """
-Tableau Integration has been disabled on the RStudio Connect server
-
-Possible Solution:
-
-- Please ask your administrator to set `Tableau.TableauIntegrationEnabled` = `true` within `rstudio-connect.gcfg` file on the RStudio Connect server.
-""",
-}
+    if len(message_list) != 0:
+        messages = "\n\n---\n\n".collapse(message_list)
+        return messages
+    else:
+        return None
