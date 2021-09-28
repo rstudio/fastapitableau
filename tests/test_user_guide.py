e46f39d70644f68ea7f7b24c935f0f834e129186
Toph Allen <toph.allen@gmail.com>
2021-09-28 11:37:10 -0400

    Remove pytest_subtests

--- tests/test_user_guide.py
+++ tests/test_user_guide.py
@@ -1,26 +1,26 @@
 import pytest
-import pytest_subtests  # type: ignore[import]
 
 import fastapitableau.user_guide as user_guide
 
-subtests = pytest_subtests.subtests
+
+@pytest.mark.parametrize(
+    "python_type,expected",
+    [
+        ("List[bool]", "Boolean"),
+        ("List[str]", "String"),
+        ("List[int]", "Integer (whole number)"),
+        ("List[float]", "Real (decimal number)"),
+        ("", "String"),
+        ("bool", "Boolean"),
+        ("str", "String"),
+        ("int", "Integer (whole number)"),
+        ("float", "Real (decimal number)"),
+    ],
+)
+def test_tableau_name_for_python_type2(python_type, expected):
+    assert user_guide.tableau_name_for_python_type(python_type) == expected
 
 
-def test_tableau_name_for_python_type(subtests):
-    test_values = {
-        "List[bool]": "Boolean",
-        "List[str]": "String",
-        "List[int]": "Integer (whole number)",
-        "List[float]": "Real (decimal number)",
-        "": "String",
-        "bool": "Boolean",
-        "str": "String",
-        "int": "Integer (whole number)",
-        "float": "Real (decimal number)",
-    }
-    for python, expected in test_values.items():
-        with subtests.test(msg=python, python=python, expected=expected):
-            assert user_guide.tableau_name_for_python_type(python) == expected
-
+def test_custom_type_error():
     with pytest.raises(TypeError):
         user_guide.tableau_name_for_python_type("CustomType")
