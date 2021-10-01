9fbf4b398499c5978eac31f933cb07625e4f0879
Toph Allen <toph.allen@gmail.com>
2021-10-01 16:25:31 -0400

    First draft of Pydantic support

--- fastapitableau/routing.py
+++ fastapitableau/routing.py
@@ -1,13 +1,45 @@
 import json
-from typing import Callable, Dict
+from typing import Any, Callable, Dict, Optional
 
 from fastapi import Request, Response
+from fastapi.dependencies.utils import request_body_to_args
+from fastapi.exceptions import RequestValidationError
+from fastapi.openapi.utils import get_flat_models_from_routes
 from fastapi.routing import APIRoute
+from pydantic.error_wrappers import ErrorWrapper
+from pydantic.schema import field_schema, get_model_name_map
 
-from .utils import replace_dict_keys
+from .utils import remove_prefix, replace_dict_keys
 
 
 class TableauRoute(APIRoute):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._body_schema: Optional[Dict[str, Any]] = None
+
+    def get_body_field_schema(self):
+        # schema, definitions, nested models
+        route_flat_models = get_flat_models_from_routes([self])
+        route_model_name_map = get_model_name_map(route_flat_models)
+        body_field_schema = field_schema(
+            self.body_field, model_name_map=route_model_name_map
+        )
+        return body_field_schema
+
+    @property
+    def body_schema(self):
+        if not self._body_schema:
+            f_schema, f_definitions, f_nested_models = self.get_body_field_schema()
+
+            if "$ref" in f_schema.keys():
+                ref = remove_prefix(f_schema["$ref"], "#/definitions/")
+                definition = f_definitions[ref]
+            else:
+                definition = f_schema
+        else:
+            definition = self._body_schema
+        return definition
+
     def get_route_handler(self) -> Callable:
         original_route_handler = super().get_route_handler()
 
@@ -17,8 +49,17 @@ class TableauRoute(APIRoute):
 
         return custom_route_handler
 
-    async def rewrite_request_body(self, request: Request) -> Request:
-        # Extract new, rename args
+    async def validate_request_body(self, received_body: Dict[str, Any]):
+        """
+        This function tries to process the request body using FastAPI's own function, and returns False if it fails to process. It's potentially expensive, but could be useful as a stricter heuristic for deciding whether to rewrite the request body.
+        """
+        values, errors = await request_body_to_args(
+            self.dependant.body_params, received_body
+        )
+        return False if errors else True
+
+    async def rewrite_request_body(self, request: Request) -> Request:  # noqa: C901
+        # Consume entire message
         message_body = b""
         more_body = True
         while more_body:
@@ -32,28 +73,53 @@ class TableauRoute(APIRoute):
             "more_body": more_body,
         }
         try:
-            body = json.loads(event["body"])
+            received_body = json.loads(event["body"])
         except Exception as e:
-            print("Failed to parse event body as JSON: {event}")
-            print(e)
             raise e
 
-        if isinstance(body, Dict) and set(body.keys()) == {"script", "data"}:
-            data = body["data"]
-            if len(data) == 1:
-                _body = list(data.values())[0]
-            elif (
-                len(data) > 1
-                and "_arg1" in data.keys()
-                and len(self.dependant.body_params) > 0
-            ):
-                # We only perform this replacement if there are more than zero body params. Otherwise, we will pass through the `data` object with no renaming, so the endpoint can define functions in terms of Tableau arg names.
-                new_keys: Dict[str, str] = {}
-                for i, param in enumerate(self.dependant.body_params):
-                    new_keys["_arg" + str(i + 1)] = param.name
-                _body = replace_dict_keys(data, new_keys)
+        # Here, we only want to operate on Tableau requests. We have a few options.
+        # 1. Duck typing, which is what the main branch does right now. It checks to see if this a dict and contains the keys "script" and "data".
+        # 2. Validate. Try to process the body with FastAPI's args and see if it works. If it doesn't, try this method.
+        # 3. Use headers.
+        request_will_validate = await self.validate_request_body(received_body)
+        if (
+            not request_will_validate
+            and isinstance(received_body, Dict)
+            and set(received_body.keys()) == {"script", "data"}
+        ):
+            data = received_body["data"]
+
+            body_type = self.body_schema["type"]
+
+            if body_type == "array":
+
+                if len(data) == 1:
+                    _body = list(data.values())[0]
+
+                else:
+                    error = ValueError(
+                        f"The endpoint {self.name} expects to receive a list, but received a {type(data)} from Tableau."
+                    )
+                    raise RequestValidationError([ErrorWrapper(error, "body")])
+
+            elif body_type == "object":
+                properties = self.body_schema["properties"]
+
+                if len(properties) == len(data):
+                    expected_names = list(properties.keys())
+                    name_map: Dict[str, str] = {}
+                    for i, new_name in enumerate(expected_names):
+                        old_name = "_arg" + str(i + 1)
+                        name_map[old_name] = new_name
+                    _body = replace_dict_keys(data, name_map)
+                else:
+                    error = ValueError(
+                        f"The route {self.name} expects {len(properties)} arguments, but received {len(data)} from Tableau."
+                    )
+                    raise RequestValidationError([ErrorWrapper(error, "body")])
             else:
                 _body = data
+
             event["body"] = bytes(json.dumps(_body), encoding="utf-8")
 
         async def _receive():
