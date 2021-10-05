7429ce3c2e32ffb7ede9382db10100a323b6703e
Toph Allen <toph.allen@gmail.com>
2021-10-05 13:09:07 -0400

    Refactored custom route handler
    Made ensuring of request body more tightly scoped to the body itself.

--- fastapitableau/routing.py
+++ fastapitableau/routing.py
@@ -44,58 +44,52 @@ class TableauRoute(APIRoute):
         original_route_handler = super().get_route_handler()
 
         async def custom_route_handler(request: Request) -> Response:
-            _request = await self.rewrite_request_body(request)
+            event = await event_from_receive(request.receive)
+            try:
+                body = json.loads(event["body"])
+            except Exception as e:
+                print("Failed to parse event body as JSON: {event}")
+                print(e)
+                raise e
+
+            _body = await self.ensure_request_body(body)
+
+            event["body"] = bytes(json.dumps(_body), encoding="utf-8")
+
+            async def _receive():
+                return event
+
+            _request = Request(request.scope, _receive)
+
             return await original_route_handler(_request)
 
         return custom_route_handler
 
-    async def body_will_validate(self, received_body: Dict[str, Any]):
-        """
-        This function tries to process the request body using FastAPI's own function, and returns False if it fails to process. It's potentially expensive, but could be useful as a stricter heuristic for deciding whether to rewrite the request body.
-        """
-        values, errors = await request_body_to_args(
-            self.dependant.body_params, received_body
-        )
-        return False if errors else True
-
-    async def rewrite_request_body(self, request: Request) -> Request:  # noqa: C901
-        # Consume entire message
-        event = await event_from_receive(request.receive)
-        try:
-            received_body = json.loads(event["body"])
-        except Exception as e:
-            print("Failed to parse event body as JSON: {event}")
-            print(e)
-            raise e
-
+    async def ensure_request_body(self, body: Dict) -> Dict:
         # Here, we only want to operate on Tableau requests. We have a few options.
         # 1. Duck typing, which is what the main branch does right now. It checks to see if this a dict and contains the keys "script" and "data".
         # 2. Validate. Try to process the body with FastAPI's args and see if it works. If it doesn't, try this method.
         # 3. Use headers.
-        body_will_validate = await self.body_will_validate(received_body)
-        if (
-            not body_will_validate
-            and isinstance(received_body, Dict)
-            and set(received_body.keys()) == {"script", "data"}
-        ):
-            data = received_body["data"]
-
-            body_type = self.body_schema["type"]
-
-            if body_type == "array":
-
+        body_will_validate = await self.body_will_validate(body)
+        if body_will_validate:
+            _body = body
+        elif isinstance(body, Dict) and set(body.keys()) == {"script", "data"}:
+            data = body["data"]
+            if self.body_schema["type"] == "array":
+                # We can handle a single arg from Tableau, raising it up a level.
+                # Receiving multiple body params causes an error.
                 if len(data) == 1:
                     _body = list(data.values())[0]
-
                 else:
                     error = ValueError(
                         f"The endpoint {self.name} expects to receive a list, but received a {type(data)} from Tableau."
                     )
                     raise RequestValidationError([ErrorWrapper(error, "body")])
 
-            elif body_type == "object":
+            elif self.body_schema["type"] == "object":
+                # We'll rename Tableau _arg[n] arguments to the argument names we expect.
+                # We order based on argument numbering from Tableau.
                 properties = self.body_schema["properties"]
-
                 if len(properties) == len(data):
                     expected_names = list(properties.keys())
                     name_map: Dict[str, str] = {}
@@ -108,12 +102,11 @@ class TableauRoute(APIRoute):
                         f"The route {self.name} expects {len(properties)} arguments, but received {len(data)} from Tableau."
                     )
                     raise RequestValidationError([ErrorWrapper(error, "body")])
-            else:
-                _body = data
+        return _body
 
-            event["body"] = bytes(json.dumps(_body), encoding="utf-8")
-
-        async def _receive():
-            return event
-
-        return Request(request.scope, _receive)
+    async def body_will_validate(self, body: Dict[str, Any]):
+        """
+        This function tries to process the request body using FastAPI's own function, and returns False if it fails to process. It's potentially expensive, but could be useful as a stricter heuristic for deciding whether to rewrite the request body.
+        """
+        values, errors = await request_body_to_args(self.dependant.body_params, body)
+        return False if errors else True
